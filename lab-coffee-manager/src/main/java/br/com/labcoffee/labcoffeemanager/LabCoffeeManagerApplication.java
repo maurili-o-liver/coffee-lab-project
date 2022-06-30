f1cc402920476471e97493a467d4d1b784cf8d17
maurilii-o-liver <mauriliodepaulo.O@gmail.com>
2022-06-30 18:41:15 -0300

    back-end

---
+++ lab-coffee-manager/src/main/java/br/com/labcoffee/labcoffeemanager/LabCoffeeManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.labcoffee.labcoffeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCoffeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCoffeeManagerApplication.class, args);
+	}
+
+}
---
+++ lab-coffee-manager/src/test/java/br/com/labcoffee/labcoffeemanager/LabCoffeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.labcoffee.labcoffeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabCoffeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
